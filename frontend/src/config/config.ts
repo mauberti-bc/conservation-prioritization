8887c180352b7822c888dd5ed55494ac9b8a57ae
Macgregor Aubertin-Young <macgregor.aubertin-young@gov.bc.ca>
2025-09-09 13:48:25 -0700

    fix vars

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -23,26 +23,21 @@ for (const varName of requiredEnvVars) {
   }
 }
 
-const VITE_API_HOST = import.meta.env.VITE_API_HOST;
-const VITE_API_PORT = import.meta.env.VITE_API_PORT || '';
 const VITE_PREFECT_API_URL = import.meta.env.VITE_PREFECT_API_URL;
 const VITE_ZARR_STORE_PATH = import.meta.env.VITE_ZARR_STORE_PATH;
 
 const OBJECT_STORE_URL = import.meta.env.VITE_OBJECT_STORE_URL;
 const OBJECT_STORE_BUCKET_NAME = import.meta.env.VITE_OBJECT_STORE_BUCKET_NAME;
 
-const API_URL = VITE_API_HOST
-  ? VITE_API_PORT
-    ? `${VITE_API_HOST}:${VITE_API_PORT}`
-    : VITE_API_HOST
-  : (() => {
-      throw new Error('VITE_API_HOST is required.');
-    })();
+const VITE_API_URL = import.meta.env.VITE_API_URL;
+if (!VITE_API_URL) {
+  throw new Error('VITE_API_URL is required.');
+}
 
 const config: IConfig = {
-  API_HOST: ensureProtocol(API_URL, 'http://'),
+  API_HOST: ensureProtocol(VITE_API_URL, 'http://'),
   PREFECT_API_HOST: ensureProtocol(VITE_PREFECT_API_URL, 'http://'),
-  NODE_ENV: import.meta.env.NODE_ENV || 'development',
+  NODE_ENV: import.meta.env.VITE_NODE_ENV || 'development',
   VITE_APP_NODE_ENV: import.meta.env.VITE_APP_NODE_ENV || 'dev',
   S3_PUBLIC_HOST_URL: ensureProtocol(`${OBJECT_STORE_URL}/${OBJECT_STORE_BUCKET_NAME}`, 'https://'),
   ZARR_STORE_PATH: VITE_ZARR_STORE_PATH,
