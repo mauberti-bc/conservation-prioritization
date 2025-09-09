343c4fa135da33eeba6dccb8ccf75f69ccf273e6
Macgregor Aubertin-Young <macgregor.aubertin-young@gov.bc.ca>
2025-09-09 13:59:09 -0700

    fix bad var references

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,21 +1,14 @@
 import { ensureProtocol } from 'utils/util';
 
 export interface IConfig {
-  API_HOST: string;
-  PREFECT_API_HOST: string;
   NODE_ENV: string;
-  VITE_APP_NODE_ENV: string;
-  S3_PUBLIC_HOST_URL: string;
+  NODE_OPTIONS: string;
+  PREFECT_API_HOST: string;
   ZARR_STORE_PATH: string;
 }
 
-const requiredEnvVars = [
-  'VITE_API_HOST',
-  'VITE_PREFECT_API_URL',
-  'VITE_OBJECT_STORE_URL',
-  'VITE_OBJECT_STORE_BUCKET_NAME',
-  'VITE_ZARR_STORE_PATH',
-];
+// Only these env vars are required now
+const requiredEnvVars = ['VITE_NODE_ENV', 'VITE_PREFECT_API_URL', 'VITE_ZARR_STORE_PATH'];
 
 for (const varName of requiredEnvVars) {
   if (!import.meta.env[varName]) {
@@ -23,23 +16,15 @@ for (const varName of requiredEnvVars) {
   }
 }
 
+const VITE_NODE_ENV = import.meta.env.VITE_NODE_ENV;
+const VITE_NODE_OPTIONS = import.meta.env.VITE_NODE_OPTIONS;
 const VITE_PREFECT_API_URL = import.meta.env.VITE_PREFECT_API_URL;
 const VITE_ZARR_STORE_PATH = import.meta.env.VITE_ZARR_STORE_PATH;
 
-const OBJECT_STORE_URL = import.meta.env.VITE_OBJECT_STORE_URL;
-const OBJECT_STORE_BUCKET_NAME = import.meta.env.VITE_OBJECT_STORE_BUCKET_NAME;
-
-const VITE_API_URL = import.meta.env.VITE_API_URL;
-if (!VITE_API_URL) {
-  throw new Error('VITE_API_URL is required.');
-}
-
 const config: IConfig = {
-  API_HOST: ensureProtocol(VITE_API_URL, 'http://'),
+  NODE_ENV: VITE_NODE_ENV,
+  NODE_OPTIONS: VITE_NODE_OPTIONS,
   PREFECT_API_HOST: ensureProtocol(VITE_PREFECT_API_URL, 'http://'),
-  NODE_ENV: import.meta.env.VITE_NODE_ENV || 'development',
-  VITE_APP_NODE_ENV: import.meta.env.VITE_APP_NODE_ENV || 'dev',
-  S3_PUBLIC_HOST_URL: ensureProtocol(`${OBJECT_STORE_URL}/${OBJECT_STORE_BUCKET_NAME}`, 'https://'),
   ZARR_STORE_PATH: VITE_ZARR_STORE_PATH,
 };
 
