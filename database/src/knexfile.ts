bb976e4c0bc901c3522ac2159e63733e7b528073
Macgregor Aubertin-Young <macgregor.aubertin-young@gov.bc.ca>
2026-02-03 23:14:59 -0800

    wip

--- database/src/knexfile.ts
+++ database/src/knexfile.ts
@@ -42,5 +42,27 @@ export default {
     seeds: {
       directory: ['procedures']
     }
+  },
+  test: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_ADMIN,
+      password: process.env.DB_ADMIN_PASS
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'migration',
+      schemaName: 'public',
+      directory: './migrations'
+    },
+    seeds: {
+      directory: ['./seeds', 'procedures']
+    }
   }
 };
