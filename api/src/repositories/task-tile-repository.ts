c0d1561623e2b966774a0c9bf48661b314f6bafb
earmitag <emma.armitage@gov.bc.ca>
2026-02-24 14:40:21 -0800

    change 404 to 400 since 404 class doesn't exist

--- api/src/repositories/task-tile-repository.ts
+++ api/src/repositories/task-tile-repository.ts
@@ -153,9 +153,6 @@ export class TaskTileRepository extends BaseRepository {
 
     const response = await this.connection.sql(sqlStatement, TaskTile);
 
-    // if (response.rowCount < 1) {
-    //   return null;
-    // }
     if (response.rows.length < 1) {
       return null;
     }
